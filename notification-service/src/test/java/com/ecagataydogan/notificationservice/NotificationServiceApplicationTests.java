9c4e3bf819766c25b5e56ab8e018a74289157ecc
esref <ecagataydogan@gmail.com>
2024-04-05 23:51:44 +0300

    add notification-service

---
+++ notification-service/src/test/java/com/ecagataydogan/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecagataydogan.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
