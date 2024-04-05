049731c9665f41d8ac616516b1aa4e619edf1f39
esref <ecagataydogan@gmail.com>
2024-04-05 15:14:47 +0300

    initial project

---
+++ kredinbizde-service/src/main/java/com/ecagataydogan/kredinbizdeservice/KredinbizdeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecagataydogan.kredinbizdeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KredinbizdeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KredinbizdeServiceApplication.class, args);
+	}
+
+}
