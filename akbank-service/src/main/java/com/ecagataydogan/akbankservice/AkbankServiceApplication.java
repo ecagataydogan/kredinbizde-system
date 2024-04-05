1ec85d5dc89a3eb3874dd490b04a0a76bfae1a00
esref <ecagataydogan@gmail.com>
2024-04-05 23:17:20 +0300

    add akbank service .gitignore, pom.xml etc

---
+++ akbank-service/src/main/java/com/ecagataydogan/akbankservice/AkbankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecagataydogan.akbankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkbankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkbankServiceApplication.class, args);
+	}
+
+}
